fac3faf4e6b5a837e685bbec6074049a7883cf6b
Thomas Johnson <trjohnson@gmail.com>
2025-07-13 23:43:41 -0500

    finish button layout

--- calculator.py
+++ calculator.py
@@ -9,16 +9,19 @@ entryBox.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 def button_add():
     return
 
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_1 = Button(root, text="1", padx=40, pady=20, command=button_click)
+button_2 = Button(root, text="2", padx=40, pady=20, command=button_click)
+button_3 = Button(root, text="3", padx=40, pady=20, command=button_click)
+button_4 = Button(root, text="4", padx=40, pady=20, command=button_click)
+button_5 = Button(root, text="5", padx=40, pady=20, command=button_click)
+button_6 = Button(root, text="6", padx=40, pady=20, command=button_click)
+button_7 = Button(root, text="7", padx=40, pady=20, command=button_click)
+button_8 = Button(root, text="8", padx=40, pady=20, command=button_click)
+button_9 = Button(root, text="9", padx=40, pady=20, command=button_click)
+button_0 = Button(root, text="0", padx=40, pady=20, command=button_click)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_click)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_click)
+button_clear = Button(root, text="clear", padx=79, pady=20, command=button_click)
 
 #put buttons on screen
 
@@ -35,6 +38,9 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
+button_clear.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=5, column=0)
+button_equal.grid(row=5, column=1, columnspan=2)
 
 
 root.mainloop()
