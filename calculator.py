9ab98108fb4b51ac3394ec10f9688aed4ddbe450
Thomas Johnson <trjohnson@gmail.com>
2025-07-13 23:34:57 -0500

    fix button placesment

--- calculator.py
+++ calculator.py
@@ -22,17 +22,17 @@ button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
 
 #put buttons on screen
 
-button_1.grid(row=1, column=0)
-button_2.grid(row=1, column=1)
-button_3.grid(row=1, column=2)
+button_1.grid(row=3, column=0)
+button_2.grid(row=3, column=1)
+button_3.grid(row=3, column=2)
 
 button_4.grid(row=2, column=0)
 button_5.grid(row=2, column=1)
 button_6.grid(row=2, column=2)
 
-button_7.grid(row=3, column=0)
-button_8.grid(row=3, column=1)
-button_9.grid(row=3, column=2)
+button_7.grid(row=1, column=0)
+button_8.grid(row=1, column=1)
+button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
 
